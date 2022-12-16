431f049151270a6c1d1b2bf3612817d45d2b98f5
Tom Tobar <cocdetombomb@gmail.com>
2022-12-16 09:10:52 -0600

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+
+    breeds = ["mastif", "chihuahua", "corgi", "shar pei", "beagle", "french bulldog", "pug", "Pointer"]
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+                self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed.lower() not in Dog.breeds:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = breed
+            
+    
+    name = property(get_name, set_name,)
+    breed = property(get_breed, set_breed,)
+
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+
+    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job not in Person.approved_jobs:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+    
+    name = property(get_name, set_name,)
+    job = property(get_job, set_job,)
